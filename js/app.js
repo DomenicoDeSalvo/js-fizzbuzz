edc981cc97be6ce424ceccdd1d1bd04b6ff23554
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-03-06 19:45:07 +0100

    Fixes:
    Variable number chengad to output
    Useless parts from 2.3 code

--- js/app.js
+++ js/app.js
@@ -4,33 +4,29 @@
 //1 Scrittura programma che stampi i numeri da 1 a 100.
 let n = 100; //Number
 for(let i = 0; i<n; i++){
-    let number = i + 1;
+    let output = i + 1;
     
     //2 Differenziazione dei valori in base al fatto che siano divisibile per tre, cinque o entrambi i numeri.
         //2.1 Calcolo del resto quando i valori vengono divisi per tre
-        let rest3 = number % 3; //Number
+        let rest3 = output % 3; //Number
         //console.log(rest3)
 
         //2.2 Calcolo del resto quando i valori vengono divisi per cinque
-        let rest5 = number % 5; //Number
+        let rest5 = output % 5; //Number
         //console.log(rest5)
 
         //2.3 Assegnazione valori in base alla divisibilità
         if(rest3 === 0 && rest5 === 0){
-            number = ('BuzzFizz')
-            console.log(number)
+            output = ('BuzzFizz'); //String
 
         } else if(rest3 === 0){
-            number = ('Fizz')
-            console.log(number)
+            output = ('Fizz'); //String
 
         } else if(rest5 === 0){
-            number = ('Buzz')
-            console.log(number)
+            output = ('Buzz'); //String
 
-        } else {
-            console.log(number)
         }
+        console.log(output)
         
 
 }
