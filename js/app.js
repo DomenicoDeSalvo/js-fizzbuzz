89540a6d9504a766e2bb8b687d4bf93531a711e7
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-03-06 15:29:00 +0100

    Added code to app.js to acquire numbers from 1 to 100

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//TEST
+//console.log('1-2-3 prova, prova, sa, sa.')
+
+//1 Scrittura programma che stampi i numeri da 1 a 100.
+let n = 100; //Number
+for(let i = 0; i<n; i++){
+    let number = i + 1
+    console.log(number)
+}
+
+//2 Differenziazione dei valori in base al fatto che siano divisibile per tre, cinque o entrambi i numeri.
